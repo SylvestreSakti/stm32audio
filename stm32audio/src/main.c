d7bed8438e203037b283c414fc80373e551ac28a
SylvestreSakti <sylvestre.prabakaran@gmail.com>
2019-01-22 15:32:58 +0100

    improve quality of sine wave

--- stm32audio/src/main.c
+++ stm32audio/src/main.c
@@ -11,265 +11,12 @@
 
 #define NUM_SAMPLES 4096
 #define LUT_SIZE 256
-uint16_t SINE_LUT[LUT_SIZE] = {
-    0x0000,
-    0x0324,
-    0x0647,
-    0x096a,
-    0x0c8b,
-    0x0fab,
-    0x12c8,
-    0x15e2,
-    0x18f8,
-    0x1c0b,
-    0x1f19,
-    0x2223,
-    0x2528,
-    0x2826,
-    0x2b1f,
-    0x2e11,
-    0x30fb,
-    0x33de,
-    0x36ba,
-    0x398c,
-    0x3c56,
-    0x3f17,
-    0x41ce,
-    0x447a,
-    0x471c,
-    0x49b4,
-    0x4c3f,
-    0x4ebf,
-    0x5133,
-    0x539b,
-    0x55f5,
-    0x5842,
-    0x5a82,
-    0x5cb4,
-    0x5ed7,
-    0x60ec,
-    0x62f2,
-    0x64e8,
-    0x66cf,
-    0x68a6,
-    0x6a6d,
-    0x6c24,
-    0x6dca,
-    0x6f5f,
-    0x70e2,
-    0x7255,
-    0x73b5,
-    0x7504,
-    0x7641,
-    0x776c,
-    0x7884,
-    0x798a,
-    0x7a7d,
-    0x7b5d,
-    0x7c29,
-    0x7ce3,
-    0x7d8a,
-    0x7e1d,
-    0x7e9d,
-    0x7f09,
-    0x7f62,
-    0x7fa7,
-    0x7fd8,
-    0x7ff6,
-    0x7fff,
-    0x7ff6,
-    0x7fd8,
-    0x7fa7,
-    0x7f62,
-    0x7f09,
-    0x7e9d,
-    0x7e1d,
-    0x7d8a,
-    0x7ce3,
-    0x7c29,
-    0x7b5d,
-    0x7a7d,
-    0x798a,
-    0x7884,
-    0x776c,
-    0x7641,
-    0x7504,
-    0x73b5,
-    0x7255,
-    0x70e2,
-    0x6f5f,
-    0x6dca,
-    0x6c24,
-    0x6a6d,
-    0x68a6,
-    0x66cf,
-    0x64e8,
-    0x62f2,
-    0x60ec,
-    0x5ed7,
-    0x5cb4,
-    0x5a82,
-    0x5842,
-    0x55f5,
-    0x539b,
-    0x5133,
-    0x4ebf,
-    0x4c3f,
-    0x49b4,
-    0x471c,
-    0x447a,
-    0x41ce,
-    0x3f17,
-    0x3c56,
-    0x398c,
-    0x36ba,
-    0x33de,
-    0x30fb,
-    0x2e11,
-    0x2b1f,
-    0x2826,
-    0x2528,
-    0x2223,
-    0x1f19,
-    0x1c0b,
-    0x18f8,
-    0x15e2,
-    0x12c8,
-    0x0fab,
-    0x0c8b,
-    0x096a,
-    0x0647,
-    0x0324,
-    0x0000,
-    0xfcdc,
-    0xf9b9,
-    0xf696,
-    0xf375,
-    0xf055,
-    0xed38,
-    0xea1e,
-    0xe708,
-    0xe3f5,
-    0xe0e7,
-    0xdddd,
-    0xdad8,
-    0xd7da,
-    0xd4e1,
-    0xd1ef,
-    0xcf05,
-    0xcc22,
-    0xc946,
-    0xc674,
-    0xc3aa,
-    0xc0e9,
-    0xbe32,
-    0xbb86,
-    0xb8e4,
-    0xb64c,
-    0xb3c1,
-    0xb141,
-    0xaecd,
-    0xac65,
-    0xaa0b,
-    0xa7be,
-    0xa57e,
-    0xa34c,
-    0xa129,
-    0x9f14,
-    0x9d0e,
-    0x9b18,
-    0x9931,
-    0x975a,
-    0x9593,
-    0x93dc,
-    0x9236,
-    0x90a1,
-    0x8f1e,
-    0x8dab,
-    0x8c4b,
-    0x8afc,
-    0x89bf,
-    0x8894,
-    0x877c,
-    0x8676,
-    0x8583,
-    0x84a3,
-    0x83d7,
-    0x831d,
-    0x8276,
-    0x81e3,
-    0x8163,
-    0x80f7,
-    0x809e,
-    0x8059,
-    0x8028,
-    0x800a,
-    0x8000,
-    0x800a,
-    0x8028,
-    0x8059,
-    0x809e,
-    0x80f7,
-    0x8163,
-    0x81e3,
-    0x8276,
-    0x831d,
-    0x83d7,
-    0x84a3,
-    0x8583,
-    0x8676,
-    0x877c,
-    0x8894,
-    0x89bf,
-    0x8afc,
-    0x8c4b,
-    0x8dab,
-    0x8f1e,
-    0x90a1,
-    0x9236,
-    0x93dc,
-    0x9593,
-    0x975a,
-    0x9931,
-    0x9b18,
-    0x9d0e,
-    0x9f14,
-    0xa129,
-    0xa34c,
-    0xa57e,
-    0xa7be,
-    0xaa0b,
-    0xac65,
-    0xaecd,
-    0xb141,
-    0xb3c1,
-    0xb64c,
-    0xb8e4,
-    0xbb86,
-    0xbe32,
-    0xc0e9,
-    0xc3aa,
-    0xc674,
-    0xc946,
-    0xcc22,
-    0xcf05,
-    0xd1ef,
-    0xd4e1,
-    0xd7da,
-    0xdad8,
-    0xdddd,
-    0xe0e7,
-    0xe3f5,
-    0xe708,
-    0xea1e,
-    0xed38,
-    0xf055,
-    0xf375,
-    0xf696,
-    0xf9b9,
-    0xfcdc,
-};
-int freq = 600;
+#define PI 3.1415926536
+int16_t SINE_LUT[LUT_SIZE] ;
+int Fs = 96000; // sampling frequency (this is a given)
+float freq = 300;
+float phase = 0; // phase accumulator
+float ampl = 0.1;
 
 volatile uint32_t time_var1, time_var2;
 
@@ -300,13 +47,13 @@ int main(void)
             if (BUTTON) {
                 if (toggled) {
                     toggled = 0;
-                    SetAudioVolume(0xCF);
+                    // SetAudioVolume(0xCF);
                 } else {
                     toggled = 1;
-                    SetAudioVolume(0xAF);
+                    // SetAudioVolume(0xAF);
                 }
                 // Increase frequency by one half tone (*= 2^1/12)
-                freq = (int)(freq * 1.059463);
+                freq = freq * 1.059463;
 
                 while (BUTTON) {
                 };
@@ -341,14 +88,15 @@ static void AudioCallback(void* context, int buffer)
     }
 
     // User code BEGIN
-
-    int Fs = 96000; // sampling frequency (this is a given)
-    float phase = 0; // phase accumulator
-    float deltaPhase = (float)freq / Fs * LUT_SIZE;
+    float deltaPhase = freq / Fs * LUT_SIZE;
 
     // Fill samples array with sine values
     for (int i = 0; i < NUM_SAMPLES; i++) {
-        samples[i] = SINE_LUT[(int)phase];
+    	float lowerSample = SINE_LUT[(int) phase];
+    	float upperSample = SINE_LUT[(int) phase + 1];
+    	float x = phase - (int) phase;
+    	float sample = lowerSample + x * (upperSample - lowerSample);
+        samples[i] = (int)sample;
         phase += deltaPhase;
         // Keep phase between 0 and 256 (excluded)
         if (phase >= (float)LUT_SIZE) {
@@ -363,6 +111,13 @@ static void AudioCallback(void* context, int buffer)
 
 void init()
 {
+	//Computing SINE TABLE
+	double theta;
+	for(int i=0;i<=LUT_SIZE;i++){
+		theta = 2 * ((double)i) * PI / 256;
+		SINE_LUT[i]=(int)((sin(theta)) / 2 * 32767);
+	}
+
     GPIO_InitTypeDef GPIO_InitStructure;
     USART_InitTypeDef USART_InitStructure;
     // ---------- SysTick timer -------- //
